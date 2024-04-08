68f98267e7075b337f60a150dafce0c93000b0df
Gaurav <gauravjoshi20199@gmail.com>
2024-04-09 00:55:14 +0530

    Setting up the Hogwarts-artifacts project

---
+++ src/test/java/com/hogwarts/hogwartsartifactonline/HogwartsArtifactOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hogwarts.hogwartsartifactonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsArtifactOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

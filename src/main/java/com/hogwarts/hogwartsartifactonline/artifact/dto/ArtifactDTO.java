a5470f096826f96dba654aeb721ee9911878bb93
Gaurav <gauravjoshi20199@gmail.com>
2025-02-06 00:38:14 +0530

    Added dependency for validation. Added basic validation for artifact request params

--- src/main/java/com/hogwarts/hogwartsartifactonline/artifact/dto/ArtifactDTO.java
+++ src/main/java/com/hogwarts/hogwartsartifactonline/artifact/dto/ArtifactDTO.java
@@ -1,9 +1,17 @@
 package com.hogwarts.hogwartsartifactonline.artifact.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.hogwarts.hogwartsartifactonline.utils.constants.ArtifactConstants;
 import com.hogwarts.hogwartsartifactonline.wizard.dto.WizardDTO;
+import jakarta.validation.constraints.NotBlank;
 
-public record ArtifactDTO(Integer id,String name, String description, String imageUrl,
+public record ArtifactDTO(
+                          @NotBlank(message = ArtifactConstants.ARTIFACT_NAME_REQUIRED)
+                          String name,
+                          @NotBlank(message = ArtifactConstants.ARTIFACT_DESC_REQUIRED)
+                          String description,
+                          @NotBlank(message = ArtifactConstants.ARTIFACT_IMAGE_URL_REQUIRED)
+                          String imageUrl,
                           @JsonInclude(JsonInclude.Include.NON_NULL)
                           WizardDTO wizard) {
 }
